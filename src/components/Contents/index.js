34221118cdb73d0a8101c37a00a7df07e5fc3526
Shuvradeb <shuvradeb.saha@ntb.no>
2022-03-24 16:56:56 +0600

    Fixed path reload issue.

--- src/components/Contents/index.js
+++ src/components/Contents/index.js
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Link, Route } from "react-router-dom";
 
 import { About, Academic } from "./contents";
 
 const ROUTES = Object.freeze({
-  ABOUT: "about",
-  ACADEMIC: "academic",
+  ABOUT: "/",
+  ACADEMIC: "/academic",
 });
 
 const SELECTED_COLOR = "#fffbfb";
 
 export default function Content() {
   const [route, setRoutes] = useState(ROUTES.ABOUT);
+
+  useEffect(() => {
+    const hashPath = window.location.hash;
+    const route = hashPath.substring(1, hashPath.length);
+    setRoutes(route);
+  });
 
   return (
     <div className="contents">
