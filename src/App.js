e0f83cfc6e64eaf95ed9740fcde7eb242cd1dc9c
Shuvradeb <shuvradeb.saha@ntb.no>
2022-10-20 18:08:51 +0600

    Wanna test if gh-pages can consume rest api.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import "./scss/app.scss";
 import { Header, Contents, Footer } from "./components";
 
 function App() {
+  fetch("https://reqres.in/api/users/2")
+    .then((response) => response.json())
+    .then((data) => console.log("Data:", data));
+
   return (
     <div className="App">
       <Header />
